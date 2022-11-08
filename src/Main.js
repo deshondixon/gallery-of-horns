d2e6ba5735c29dabd64b5dd8d51edbd5cdc194ad
deshondixon <deshondixon@gmail.com>
2022-11-08 11:26:50 -0800

    finallyfixed

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
+import data from './data.json';
 
 class Main extends React.Component {
   render() {
+    
+    let beastArray = [];
+    data.forEach((beast, idx) => {
+      beastArray.push(
+      <HornedBeast
+        imageURL={beast.image_url}
+        title={beast.title}
+        description={beast.description}
+      key={idx}
+      />
+      )
+    });
     return (
       <main>
-        <article>
-
-          <HornedBeast title="UniWhal" description="A unicorn and a narwhal nuzzling their horns" imageURL="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
-
-          <HornedBeast title="Rhino Family" description="Parent rhino with two babies" imageURL="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"/>
-
-          <HornedBeast title="Unicorn Head" description="Someone wearing a very silly unicorn head mask" imageURL="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"/>
-
-        </article>
+        {beastArray}
       </main>
     )
   }
