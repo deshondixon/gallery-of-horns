091f144c5699707daea00e18b8bc0cec6ce700bc
deshondixon <deshondixon@gmail.com>
2022-11-07 21:29:45 -0800

    lab 1

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>&copy; DeShon Dixon 2022</footer>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/HornedBeast.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+      </article> 
+      )
+  }
+}
+
+export default HornedBeast;
